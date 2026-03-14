91692943c260a0e1163ed853220e83554744f015
German Reyes <german.951026@gmail.com>
2026-03-13 19:20:05 -0500

    se actualiza con tailwind y cambios en el carrito

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        teal: { DEFAULT:'#0D9488', light:'#14B8A6', pale:'#CCFBF1', dark:'#0F766E' },
+        brand: { bg:'#F0FDF9', border:'#99F6E4' }
+      }
+    }
+  },
+  plugins: []
+}
